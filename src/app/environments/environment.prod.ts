0f3ff6b66d3e5b234e8cdd0688956cb7d1434bce
Riosberg Dutra <riosbergduttra@gmail.com>
2026-06-06 07:42:27 -0300

    chore: add environment configuration

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.seudominio.com.br',
+  wsUrl: 'wss://api.seudominio.com.br/ws/notifications',
+};
+ 
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  wsUrl: 'ws://localhost:8080/ws/notifications',
+};
+ 
